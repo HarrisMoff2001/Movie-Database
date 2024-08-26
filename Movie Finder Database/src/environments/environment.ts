b0e4a2e91e6103b489238e4e0d8a7ac5dad6d8a5
HarrisMoff2001 <harrismoff1234@gmail.com>
2024-08-26 02:54:14 +0100

    Add files via upload

---
+++ Movie Finder Database/src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  firebase: {
+    projectId: 'assignment2-45537',
+    appId: '1:850268906317:web:47a1d2c9d29fb86e718d04',
+    databaseURL: 'https://assignment2-45537-default-rtdb.europe-west1.firebasedatabase.app',
+    storageBucket: 'assignment2-45537.appspot.com',
+    apiKey: 'AIzaSyBu8uxvpOtSREWw6NzhLJdNsin3LD44jwY',
+    authDomain: 'assignment2-45537.firebaseapp.com',
+    messagingSenderId: '850268906317',
+    measurementId: 'G-04P62JQRRR',
+  },
+};
